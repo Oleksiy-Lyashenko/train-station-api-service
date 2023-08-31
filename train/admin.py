42543814e0556f964c06b187e30cba67e30612e9
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2023-08-31 22:44:35 +0300

    Create admin panel for train app

--- train/admin.py
+++ train/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from train.models import Crew, TrainType, Train, Station, Route, Journey, Order, Ticket
+
+admin.site.register(Crew)
+admin.site.register(TrainType)
+admin.site.register(Train)
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(Journey)
+admin.site.register(Order)
+admin.site.register(Ticket)
+
+
+
